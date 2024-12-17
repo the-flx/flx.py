c3163693826f452cc93f589772be3a49bbabb7b8
JenChieh <jcs090218@gmail.com>
2024-12-17 00:50:25 -0800

    Add flx

---
+++ flx/flx.py
@@ -0,0 +1,15 @@
+# ========================================================================
+# $File: flx.py $
+# $Date: 2024-12-16 22:02:28 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2024 by Shen, Jen-Chieh $
+# ========================================================================
+
+def main():
+    """Program Entry point.
+    """
+
+if __name__ == "__main__":
+    main()
