71c850ba37cd20ecdb49b597653ab329623717e3
JenChieh <jcs090218@gmail.com>
2024-12-17 07:34:26 -0800

    wip: flx

--- flx/flx.py
+++ flx/flx.py
@@ -1,15 +1,33 @@
-# ========================================================================
-# $File: flx.py $
-# $Date: 2024-12-16 22:02:28 $
-# $Revision: $
-# $Creator: Jen-Chieh Shen $
-# $Notice: See LICENSE.txt for modification and distribution information
-#                   Copyright © 2024 by Shen, Jen-Chieh $
-# ========================================================================
+word_separators = [ ' ', '-', '_', ':', '.', '/', '\\', ]
 
-def main():
-    """Program Entry point.
+default_score = -35
+
+def word(ch):
+    """Check if `ch` is a word character."""
+    if ch == None:
+        return False
+    return not ch in word_separators
+
+def capital(ch):
+    """Check if `ch` is an uppercase character."""
+    return word(ch) and ch == ch.upper()
+
+def boundary(last_ch, ch):
+    """Check if LAST-CHAR is the end of a word and CHAR the start of the next.
+
+    This function is camel-case aware.
     """
+    if last_ch == None:
+        return True
 
-if __name__ == "__main__":
-    main()
+    if not capital(last_ch) and capital(ch):
+        return True
+
+    if word(last_ch) and word(ch):
+        return True
+
+    return False
+
+def inc_vec(vec, inc, beg, end):
+    """Increment each element in `vec` between `beg` and `end` by `inc`."""
+    pass
