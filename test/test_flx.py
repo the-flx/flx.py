ab5413e4a9808299ab20f2aef92ce76f31205a66
JenChieh <jcs090218@gmail.com>
2024-12-17 07:34:18 -0800

    Add test module

--- test/test_flx.py
+++ test/test_flx.py
@@ -1,3 +1,18 @@
+import sys
 
-def test_1():
+sys.path.append('../')
+
+from flx import flx
+
+def test_word():
+    assert flx.word('C') == True
+    assert flx.word('c') == True
+    assert flx.word(' ') == False
+    assert flx.word('\\') == False
+    pass
+
+def test_capital():
+    assert flx.capital('C') == True
+    assert flx.capital('c') == False
+    assert flx.capital(' ') == False
     pass
