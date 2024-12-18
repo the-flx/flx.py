56701d8570cb1bc4122a28615cbc156fca567fbb
JenChieh <jcs090218@gmail.com>
2024-12-17 16:33:24 -0800

    add project metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="the-flx",
-    version="0.1.0",
-    packages=find_packages(),
+    description="Rewrite emacs-flx in Python",
+    version="0.1.1",
+    url="https://github.com/the-flx/flx.py",
+    project_urls={
+        "Source Code": "https://github.com/the-flx/flx.py",
+    },
+    author="Jen-Chieh Shen",
+    author_email="jcs090218@gmail.com",
+    license="MIT",
+    python_requires=">=3.9",
+    packages=["flx"],
     install_requires= [],
     )
